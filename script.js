c3035a44fb30cfa604f36acabf3a77198ea36df2
petrosly <petroslymper@gmail.com>
2020-10-14 17:20:41 +0300

    Updated about, projects, contact sections

---
+++ script.js
@@ -0,0 +1,101 @@
+/* Handling hover on projects*/
+document.querySelector(".project-chat").addEventListener("mouseover",()=>{
+  document.querySelector(".chat-hover").style.display = "block";
+});
+
+document.querySelector(".project-chat").addEventListener("mouseout",()=>{
+  document.querySelector(".chat-hover").style.display = "none";
+});
+
+document.querySelector(".project-hotel").addEventListener("mouseover",()=>{
+  document.querySelector(".hotel-hover").style.display = "block";
+});
+
+document.querySelector(".project-hotel").addEventListener("mouseout",()=>{
+  document.querySelector(".hotel-hover").style.display = "none";
+});
+
+document.querySelector(".project-game").addEventListener("mouseover",()=>{
+  document.querySelector(".game-hover").style.display = "block";
+});
+
+document.querySelector(".project-game").addEventListener("mouseout",()=>{
+  document.querySelector(".game-hover").style.display = "none";
+});
+
+document.querySelector(".project-portfolio").addEventListener("mouseover",()=>{
+  document.querySelector(".portfolio-hover").style.display = "block";
+});
+
+document.querySelector(".project-portfolio").addEventListener("mouseout",()=>{
+  document.querySelector(".portfolio-hover").style.display = "none";
+});
+/* Modal behavior*/
+const expandChat  = document.querySelector(".expand-chat-hover");
+const expandHotel = document.querySelector(".expand-hotel-hover");
+const expandGame  = document.querySelector(".expand-game-hover");
+const expandPortfolio = document.querySelector(".expand-portfolio-hover");
+const modal = document.querySelector(".modal");
+const modalTitle  = document.querySelector(".modal-title");
+const modalDescription  = document.querySelector(".modal-description");
+const modalTechnologies = document.querySelector(".modal-technologies");
+const modalImage  = document.querySelector(".modal-image");
+const modalFirstButton = document.querySelector(".first-button");
+const modalSecondButton = document.querySelector(".second-button");
+
+function closeModal(){
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+expandChat.addEventListener("click", ()=>{
+  modal.style.display = "block";
+  modalSecondButton.style.display = "block";
+  modalTitle.innerHTML= "Online Chatroom";
+  modalDescription.innerHTML = "This is a web app that allows its users to have private conversations with anyone online. It stores and loads old messages, plus it offers an authentication mechanism through email!";
+  modalTechnologies.innerHTML = "<li>Technologies used for this project:&nbsp;</li><li>Node.js,</li><li>&nbsp;Express,</li><li>&nbsp;Socket.io,</li><br><li>MongoDB,</li><li>&nbsp;HTML5,</li><li>&nbsp;CSS3,</li><li>&nbsp;ES6,</li><li>&nbsp;Heroku</li>";
+  modalImage.setAttribute("src","images/laptop-chat.jpg");
+  modalImage.style.top = "-13vh";
+  modalFirstButton.innerHTML = "Get Source";
+  modalSecondButton.innerHTML = "View Demo";
+});
+
+expandHotel.addEventListener("click", ()=>{
+  modal.style.display = "block";
+  modalTitle.innerHTML = "Hotel Website";
+  modalDescription.innerHTML = "This is a static website that is used to represent this hotel's services. There are many more details inside the website!";
+  modalTechnologies.innerHTML = "<li>Technologies used for this project:&nbsp;</li><li>HTML5,</li><li>&nbsp;CSS3,</li><li>&nbsp;ES6</li>";
+  modalImage.setAttribute("src","images/laptop-hotel.jpg");
+  modalImage.style.top = "-13vh";
+  modalFirstButton.innerHTML = "View Demo"
+  modalSecondButton.style.display = "none";
+});
+
+expandGame.addEventListener("click", ()=>{
+  modal.style.display = "block";
+  modalSecondButton.style.display = "block";
+  modalTitle.innerHTML  =  "Traffic Education Game";
+  modalDescription.innerHTML  = "It is a game whose goal is to educate children of smaller ages on the traffic matters. It is equipped with a database that is used for an analysis on the scores of the user. The analysis is then used to switch the focus on areas that the user lacks knowledge and also make the application meet each user's needs.";
+  modalTechnologies.innerHTML = "<li>Technologies used for this project:&nbsp;</li><li>C#,</li><li>&nbsp;Unity,</li><li>&nbsp;SQLite</li>";
+  modalImage.setAttribute("src","images/laptop-game.jpg");
+  modalImage.style.top = "-17vh";
+  modalFirstButton.innerHTML = "Get Source";
+  modalSecondButton.innerHTML = "Download Game";
+});
+
+expandPortfolio.addEventListener("click", ()=>{
+  modal.style.display = "block";
+  modalSecondButton.style.display = "block";
+  modalTitle.innerHTML = "Personal Portfolio Website";
+  modalDescription.innerHTML = "This is the page you are currently on! It is a static website that is used to display my projects!";
+  modalTechnologies.innerHTML = "<li>Technologies used for this project:&nbsp;</li><li>HTML5,</li><li>&nbsp;CSS3,</li><li>&nbsp;ES6,</li><li>&nbsp;Netlify</li>";
+  modalImage.setAttribute("src","images/laptop-portfolio.jpg");
+  modalImage.style.top = "-13vh";
+  modalFirstButton.innerHTML = "Get Source";
+  modalSecondButton.innerHTML = "View Demo";
+});
