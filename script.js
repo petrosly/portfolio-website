68f1764a3f303c52ca8cea30a957309cf3211236
petrosly <petroslymper@gmail.com>
2020-10-18 14:38:16 +0300

    Added alt attributes on modal images

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ function chatModal(){
   modalDescription.innerHTML = "This is a web app that allows its users to have private conversations with anyone online. It stores and loads old messages, plus it offers an authentication mechanism through email!";
   modalTechnologies.innerHTML = "<li>Technologies used for this project:&nbsp;</li><li>Node.js,</li><li>&nbsp;Express,</li><li>&nbsp;Socket.io,</li><li>&nbsp;MongoDB,</li><li>&nbsp;HTML5,</li><li>&nbsp;CSS3,</li><li>&nbsp;ES6,</li><li>&nbsp;Heroku</li>";
   modalImage.setAttribute("src","images/laptop-chat.jpg");
+  modalImage.setAttribute("alt","Screenshot of chat application inside laptop mockup");
   modalFirstButton.innerHTML = "Get Source";
   modalFirstButton.onclick = ()=>{ window.open("https://github.com/petrosly/online-chatroom-v2", "_blank");};
   modalSecondButton.innerHTML = "View Demo";
@@ -87,6 +88,7 @@ function hotelModal(){
   modalDescription.innerHTML = "This is a static website that is used to represent this hotel's services. There are many more details inside the website!";
   modalTechnologies.innerHTML = "<li>Technologies used for this project:&nbsp;</li><li>HTML5,</li><li>&nbsp;CSS3,</li><li>&nbsp;ES6</li>";
   modalImage.setAttribute("src","images/laptop-hotel.jpg");
+  modalImage.setAttribute("alt","Screenshot of hotel website inside laptop mockup");
   modalFirstButton.innerHTML = "View Demo"
   modalSecondButton.style.display = "none";
 }
@@ -98,6 +100,7 @@ function gameModal(){
   modalDescription.innerHTML  = "It is a game whose goal is to educate children of smaller ages on the traffic matters. It is equipped with a database that is used for an analysis on the scores of the user. The analysis is then used to switch the focus on areas that the user lacks knowledge and also make the application meet each user's needs.";
   modalTechnologies.innerHTML = "<li>Technologies used for this project:&nbsp;</li><li>C#,</li><li>&nbsp;Unity,</li><li>&nbsp;SQLite</li>";
   modalImage.setAttribute("src","images/laptop-game.jpg");
+  modalImage.setAttribute("alt","Screenshot of my game inside laptop mockup");
   modalFirstButton.innerHTML = "Get Source";
   modalSecondButton.innerHTML = "Download Game";
 }
@@ -109,6 +112,7 @@ function portfolioModal(){
   modalDescription.innerHTML = "This is the page you are currently on! It is a static website that is used to display my projects!";
   modalTechnologies.innerHTML = "<li>Technologies used for this project:&nbsp;</li><li>HTML5,</li><li>&nbsp;CSS3,</li><li>&nbsp;ES6,</li><li>&nbsp;Netlify</li>";
   modalImage.setAttribute("src","images/laptop-portfolio.jpg");
+  modalImage.setAttribute("alt","Screenshot of portfolio website inside laptop mockup");
   modalFirstButton.innerHTML = "Get Source";
   modalSecondButton.innerHTML = "View Demo";
 }
